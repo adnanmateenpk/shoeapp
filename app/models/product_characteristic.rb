70fc3f62075df74435b900c9e47db3fda5e3b36f
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2014-12-30 12:05:23 -0430

    indentation and comments commit

--- app/models/product_characteristic.rb
+++ app/models/product_characteristic.rb
@@ -1,6 +1,12 @@
 class ProductCharacteristic < ActiveRecord::Base
-belongs_to :product
-validates :price, :numericality => true, :presence => true,
-validates :size, :presence => true,
-validates :color, :presence => true,
+  #relations
+  belongs_to :product
+
+  #validations
+  validates :price, :numericality => true, :presence => true,
+
+  validates :size, :presence => true,
+
+  validates :color, :presence => true,
+
 end
