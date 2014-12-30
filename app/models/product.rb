fab8e0609653b4c90cd7245d45f651aefcecf148
adnanmateenpk <adnanmateen@outlook.com>
2014-12-30 21:30:59 +0500

    indentation in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
 class Product < ActiveRecord::Base
-has_many :product_characteristics
-validates :model_name, :presence => true,
-:length => { :maximum => 25 }
-validates :sku, :uniqueness => true, :presence => true
-validates :position, :numericality =>{:only_integer => true}, :presence => true
+	
+	#relations
+	has_many :product_characteristics
+	
+
+	#validations
+	validates :model_name,	:presence => true,
+							:length => { :maximum => 25 }
+	validates :sku, 		:uniqueness => true, 
+							:presence => true
+	validates :position, 	:numericality =>{:only_integer => true}, 
+							:presence => true
 end
