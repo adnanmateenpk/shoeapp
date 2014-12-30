d7790e2019c3d0cc38ab834f253eae52eeded3dc
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2014-12-30 09:29:28 -0430

    product controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/create'
+
+  get 'products/new'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/delete'
+
+  get 'products/destroy'
+
   devise_for :users
   root to: 'shop#index'
 
