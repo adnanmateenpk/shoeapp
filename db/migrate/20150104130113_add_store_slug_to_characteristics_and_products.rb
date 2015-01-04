948ca0c13fef3e2befbf9a80a848056b6a497c0d
adnanmateenpk <adnanmateen@outlook.com>
2015-01-04 18:08:25 +0500

    migration fix

--- db/migrate/20150104130113_add_store_slug_to_characteristics_and_products.rb
+++ db/migrate/20150104130113_add_store_slug_to_characteristics_and_products.rb
@@ -2,9 +2,13 @@ class AddStoreSlugToCharacteristicsAndProducts < ActiveRecord::Migration
   def up
     add_column("products","slug", :string)
     add_index("products","slug")
+    add_column("product_characteristics","slug", :string)
+    add_index("product_characteristics","slug")
   end
 
   def down
+  	remove_index("product_characteristics","slug")
+    remove_column("product_characteristics","slug")
     remove_index("products","slug")
     remove_column("products","slug")
   end
