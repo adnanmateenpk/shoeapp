9afd09b3f34e4c831154c09f3c0fd78cda0cc3a9
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2015-01-07 11:00:26 -0430

    move create and update methods slug to private in controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -55,7 +55,12 @@ before_action :authenticate_admin_user!
 
 private
   def product_params
-    params.require(:product).permit(:mod_name,:sku,:position,:description,:status)
+    if @params[:slug].blank
+        @params[:slug] = @params[:mod_name].parameterize
+    else
+        @params[:slug] = @params[:slug].parameterize
+    end
+    params.require(:product).permit(:mod_name,:sku,:position,:description,:status,:slug)
   end
 
 end
