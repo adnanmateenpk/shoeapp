3318961b2a31d207221f8e60ec22a4a54c41d97d
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2015-01-07 14:23:27 -0430

    added methods to characteristics controller

--- app/controllers/characteristics_controller.rb
+++ app/controllers/characteristics_controller.rb
@@ -2,17 +2,22 @@ class CharacteristicsController < ApplicationController
   layout 'admin'
   before_action :authenticate_admin_user!
   def index
+    @product_slug = params[:product_slug]
   end
 
   def show
+    @product_slug = params[:product_slug]
   end
 
   def new
+    @product_slug = params[:product_slug]
   end
 
   def edit
+    @product_slug = params[:product_slug]
   end
 
   def delete
+    @product_slug = params[:product_slug]
   end
 end
