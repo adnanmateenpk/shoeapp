d6e38ae223ad0f700dbe8b593f1c8f3b87354c34
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2015-01-08 16:03:50 -0430

    migration error after doing change to create order migration

--- db/migrate/20150108201513_create_orders_product_characteristics.rb
+++ db/migrate/20150108201513_create_orders_product_characteristics.rb
@@ -1,9 +1,9 @@
 class CreateOrdersProductCharacteristics < ActiveRecord::Migration
   def change
-    create_table :orders_product_characteristics, :id=> false do |t|
+    create_table :product_characteristics_orders, :id=> false do |t|
       t.integer :product_characteristic_id
       t.integer :order_id
     end
-    add_index :order_product_characteristics, [order_id, product_characterisric_id]
+    add_index :product_characteristics_orders, [order_id, product_characterisric_id]
   end
 end
