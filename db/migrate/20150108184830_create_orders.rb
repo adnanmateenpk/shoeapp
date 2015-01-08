a45e36eac15617e284b411d61ab3c685ff95487f
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2015-01-08 14:38:14 -0430

    added to orders migration

--- db/migrate/20150108184830_create_orders.rb
+++ db/migrate/20150108184830_create_orders.rb
@@ -1,8 +1,13 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-
+      t.string :tracking_no
+      t.integer :user_id
+      t.integer :status
+      t.float :total_price
       t.timestamps null: false
     end
   end
+  add_index :orders, :user_id
+  add_index :orders, :tracking_no
 end
