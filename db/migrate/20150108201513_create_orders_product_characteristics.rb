db2bc1b961931cc9091a72f075d4d568c049c1bd
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2015-01-08 15:57:05 -0430

    added field to join table orders

--- db/migrate/20150108201513_create_orders_product_characteristics.rb
+++ db/migrate/20150108201513_create_orders_product_characteristics.rb
@@ -1,7 +1,9 @@
 class CreateOrdersProductCharacteristics < ActiveRecord::Migration
   def change
-    create_table :orders_product_characteristics do |t|
-    
+    create_table :orders_product_characteristics, :id=> false do |t|
+      t.integer :product_characteristic_id
+      t.integer :order_id
     end
+    add_index :order_product_characteristics, [order_id, product_characterisric_id]
   end
 end
