44f645ca5ba0baef35705c3c2f6ebfbfa5967de7
adnanmateenpk <adnanmateen@outlook.com>
2015-01-09 04:14:35 +0500

    order.rb fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   #relations
   belongs_to :customer, :class_name => "User",:foreign_key => "user_id"
-  has_many :char_orders, :foreign_key => "order_id"
+  has_many :chars_orders, :foreign_key => "order_id"
   has_many :product_characteristics, :through => :chars_orders
   #validations
   validates :tracking_no, :presence => true, :uniqueness => true
