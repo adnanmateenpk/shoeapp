8ace15e3fe251a6dba767ff3257f01eb654362e4
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2015-01-08 18:15:15 -0430

    recommit

--- db/migrate/20150108220106_create_chars_orders.rb
+++ db/migrate/20150108220106_create_chars_orders.rb
@@ -1,8 +1,11 @@
 class CreateCharsOrders < ActiveRecord::Migration
   def change
     create_table :chars_orders do |t|
-
+      t.integer :chars_id
+      t.integer :order_id
+      t.integer :quantity
       t.timestamps null: false
     end
+    add_index :chars_orders,[:chars_id, :order_id]
   end
 end
