09d71f657217f2b0d4e227abe7f1c6e57c55de76
adnanmateenpk <adnanmateen@outlook.com>
2015-01-09 01:53:51 +0500

    migration fix

--- db/migrate/20150108201513_create_orders_product_characteristics.rb
+++ db/migrate/20150108201513_create_orders_product_characteristics.rb
@@ -1,9 +1,10 @@
 class CreateOrdersProductCharacteristics < ActiveRecord::Migration
   def change
-    create_table :product_characteristics_orders, :id=> false do |t|
-      t.integer :product_characteristic_id
+    create_table :chars_orders, :id => false do |t|
+      t.integer :chars_id
       t.integer :order_id
     end
-    add_index :product_characteristics_orders, [order_id, product_characterisric_id]
+    add_index :chars_orders, [:chars_id,:order_id]
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108184830) do
+ActiveRecord::Schema.define(version: 20150108201513) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20150108184830) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "chars_orders", id: false, force: :cascade do |t|
+    t.integer "chars_id", limit: 4
+    t.integer "order_id", limit: 4
+  end
+
+  add_index "chars_orders", ["chars_id", "order_id"], name: "index_chars_orders_on_chars_id_and_order_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
     t.string   "tracking_no", limit: 255
