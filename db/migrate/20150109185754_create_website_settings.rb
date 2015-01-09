50b21385e740926b70139fad5b93cbe3f6afe379
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2015-01-09 14:41:39 -0430

    generate website settings models, added fields to migrate file of model

---
+++ db/migrate/20150109185754_create_website_settings.rb
@@ -0,0 +1,15 @@
+class CreateWebsiteSettings < ActiveRecord::Migration
+  def change
+    create_table :website_settings do |t|
+      t.string :title
+      t.string :website_url
+      t.string :email
+      t.string :url
+      t.string :meta_keyword
+      t.string :meta_description
+      t.string :stripe_secret_key
+      t.string :stripe_publishable_key
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109183940) do
+ActiveRecord::Schema.define(version: 20150109185754) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -104,5 +104,18 @@ ActiveRecord::Schema.define(version: 20150109183940) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "website_settings", force: :cascade do |t|
+    t.string   "title",                  limit: 255
+    t.string   "website_url",            limit: 255
+    t.string   "email",                  limit: 255
+    t.string   "url",                    limit: 255
+    t.string   "meta_keyword",           limit: 255
+    t.string   "meta_description",       limit: 255
+    t.string   "stripe_secret_key",      limit: 255
+    t.string   "stripe_publishable_key", limit: 255
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+  end
 
 end
