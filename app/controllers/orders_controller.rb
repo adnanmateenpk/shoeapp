ca4c2d064297bb56be2f2080ac23e74c557ba7da
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2015-01-09 11:01:50 -0430

    added code to order controller, created table for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
   layout 'admin'
-  before_action :authenticate_admin_user!  
+  before_action :authenticate_admin_user!
   def index
+    @orders = Order.all
   end
 
   def edit
