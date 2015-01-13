181be4d75579408efdcd5c93256de5326752e3fc
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2015-01-13 12:47:43 -0430

    asd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,19 @@ module ApplicationHelper
       :locals => {:object => object})
   end
 
-	def upload_files_custom
-		Dir.pwd
+	def upload_files_custom(file)
+		name = santize_filename(file["image"].original_filename)
+		directory = "public/"+Setting.all.first.upload_path
+		path = File.join(directory,name)
+		File.open(path,"wb"){|f| f.write(upload['image'].read) }
 	end
+
+	def sanitize_filename(file_name)
+		# get only the filename, not the whole path (from IE)
+		just_filename = File.basename(file_name)
+		# replace all none alphanumeric, underscore or perioids
+		# with underscore
+		just_filename.sub(/[^\w\.\-]/,'-')
+	end
+
 end
