a694eb15feccbb71194e32eb165c9a563f460568
Hector Garcia <hga@Hectors-MacBook-Pro.local>
2015-01-15 14:35:10 -0430

    fix single view issue with radio button, change details views minor

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -10,12 +10,12 @@ class ShopController < ApplicationController
 
   def add_to_cart
     if session["products"].nil?
-       initialize_shopping_cart
+      initialize_shopping_cart
     end
     product_count = session["products"].length
     session["products"][product_count] = Hash.new
     session["products"][product_count]["id"]=params[:selected]
-    if params["quantity"+params[:selected]].blank?
+    if params["quantity"+params[:selected]].nil? || params["quantity"+params[:selected]].blank?
       session["products"][product_count]["quantity"] = 1
     else
       session["products"][product_count]["quantity"] = params["quantity"+params[:selected]]
